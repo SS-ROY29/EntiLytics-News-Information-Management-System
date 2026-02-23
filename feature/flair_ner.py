01ba3333b97592240a6d9319ec4e245899f2ba80
MarcRGM <marcrodniemanalo@gmail.com>
2026-02-23 12:51:13 +0800

    chore: disable writing bytecode

--- feature/flair_ner.py
+++ feature/flair_ner.py
@@ -1,6 +1,9 @@
 from flair.data import Sentence
 from flair.nn import Classifier
 
+import sys
+sys.dont_write_bytecode = True
+
 def identify_entities(input):
     # Load the standard English NER model 
     # 'ner-fast' for a smaller, faster BiLSTM model if preferred
